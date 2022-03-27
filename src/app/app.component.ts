67deb0c0de261be2100446b7052a4310b4c1c872
Dayro Pataueg <dapataueg@myseneca.ca>
2022-03-27 18:39:40 -0400

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* BTI425 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Dayro Pataueg Student ID: 144908183 Date: March 26, 2022
+*
+********************************************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
