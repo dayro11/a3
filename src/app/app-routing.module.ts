3cacec63ca1eeaf73c49faf5624e8523508abb6f
Dayro Pataueg <dapataueg@myseneca.ca>
2022-03-27 18:20:24 -0400

    all steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'blog', component:BlogComponent},
+  {path:'post', component:PostComponent},
+  {path:"", redirectTo:'/home' ,pathMatch:'full'},
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
